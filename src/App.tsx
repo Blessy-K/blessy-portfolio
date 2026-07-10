eb931858778232bea35ac31149ff010136cf8229
Blessy K <23ag1a67f7@gmail.com>
2026-07-10 14:24:21 +0530

    Add certificates route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/layout/Footer";
 import Home from "./pages/Home";
+import Certificates from "./pages/Certificates";
 
 function App() {
   return (
-    <>
-      <Home />
-      <Footer />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Home />
+              <Footer />
+            </>
+          }
+        />
+
+        <Route path="/certificates" element={<Certificates />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
